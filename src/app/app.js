60f5883331e23889de2c45461d2e7a95196badf1
Kevin Mershon <kevin@slixbits.com>
2015-02-26 00:04:27 -0800

    Bootstrap website using ngBoilerplate with AngularJS and Bootstrap

---
+++ src/app/app.js
@@ -0,0 +1,24 @@
+angular.module( 'mershon-enterprises', [
+  'templates-app',
+  'templates-common',
+  'mershon-enterprises.home',
+  'ui.router'
+])
+
+.config( function myAppConfig ( $stateProvider, $urlRouterProvider ) {
+  $urlRouterProvider.otherwise( '/home' );
+})
+
+.run( function run () {
+})
+
+.controller( 'AppCtrl', function AppCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | Mershon Enterprises' ;
+    }
+  });
+})
+
+;
+
---
+++ src/app/home/home.spec.js
@@ -0,0 +1,8 @@
+describe( 'home section', function() {
+  beforeEach( module( 'mershon-enterprises.home' ) );
+
+  it( 'should have a dummy test', inject( function() {
+    expect( true ).toBeTruthy();
+  }));
+});
+
