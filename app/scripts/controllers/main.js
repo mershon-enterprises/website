c995429eb289df594214606b8a8114024bc7a5c2
Devin Leonhart <devinleonhart@gmail.com>
2015-04-08 03:52:04 -0700

    Added function for jquery scroll to contact

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,16 @@
  * Controller of the websiteApp
  */
 angular.module('websiteApp')
-  .controller('MainCtrl', function ($rootScope) {
+  .controller('MainCtrl', function ($scope, $rootScope) {
     $rootScope.routes = {
       'home': false,
       'about': false,
       'showcase': false,
       'blog': false
     };
+
+    $scope.scrollToContact = function() {
+      $('html, body').animate({scrollTop: $('#footer').offset().top}, 1000);
+    };
+
   });
