a0980002182b69181d8912de2982479848b84f2c
Devin Leonhart <devinleonhart@gmail.com>
2015-04-09 15:46:20 -0700

    Removed navbar directive because of skel's init.js behavior

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,46 @@
  * Controller of the websiteApp
  */
 angular.module('websiteApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location) {
+
+    //Tracks which route we're currently on for the "current" page tab in the
+      //navbar.
+      $scope.routes = {
+        'home': false,
+        'about': false,
+        'showcase': false,
+        'blog': false
+      };
+
+      //Set current route based on location. Helps tab persist through page
+      //refresh.
+      switch($location.path()){
+        case '/':
+          $scope.routes.home = true;
+          break;
+        case '/about':
+          $scope.routes.about = true;
+          break;
+        case '/showcase':
+          $scope.routes.showcase = true;
+          break;
+        case '/blog':
+          $scope.routes.blog = true;
+          break;
+        default:
+          $scope.routes.home = true;
+          break;
+      }
+
+      //The route set to true will have its navbar tab given
+      //the "current" class.
+      $scope.changeCurrentTab = function(key) {
+        $scope.routes.home = false;
+        $scope.routes.about = false;
+        $scope.routes.showcase = false;
+        $scope.routes.blog = false;
+        $scope.routes[key] = true;
+      };
 
     //Animate the page down to the contact form.
     $scope.scrollToContact = function() {
