1a823a19177bb48f5c2b265081487602469682d6
Devin Leonhart <devinleonhart@gmail.com>
2015-04-15 14:46:46 -0700

    Removed sass from wiredep check

--- Gruntfile.js
+++ Gruntfile.js
@@ -192,10 +192,6 @@ module.exports = function (grunt) {
               }
             }
           }
-      },
-      sass: {
-        src: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
-        ignorePath: /(\.\.\/){1,2}bower_components\//
       }
     },
 
