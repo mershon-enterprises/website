40827f643515830b12c7771f1a7f843d50dbea61
Devin Leonhart <devinleonhart@gmail.com>
2015-05-05 10:45:18 -0700

    Added bowercopy and new uglify rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -215,18 +215,18 @@ module.exports = function (grunt) {
       }
     },
 
-    // uglify: {
-    //   dist: {
-    //     files: {
-    //       '<%= yeoman.dist %>/scripts/scripts.js': [
-    //         '<%= yeoman.dist %>/scripts/scripts.js'
-    //       ]
-    //     }
-    //   }
-    // },
-    // concat: {
-    //   dist: {}
-    // },
+    uglify: {
+      dist: {
+        files: {
+          '<%= yeoman.dist %>/bower_components/skel/src/skel.js': '<%= yeoman.dist %>/bower_components/skel/src/skel.js',
+          '<%= yeoman.dist %>/bower_components/skel-layers/src/skel-layers.js': '<%= yeoman.dist %>/bower_components/skel-layers/src/skel-layers.js',
+          '<%= yeoman.dist %>/bower_components/angular-route/angular-route.js': '<%= yeoman.dist %>/bower_components/angular-route/angular-route.js',
+        }
+      }
+    },
+    concat: {
+      dist: {}
+    },
 
     imagemin: {
       dist: {
@@ -286,6 +286,19 @@ module.exports = function (grunt) {
       dist: {
         html: ['<%= yeoman.dist %>/*.html']
       }
+    },
+
+    bowercopy: {
+      options: {
+                destPrefix: '<%= yeoman.dist %>/bower_components'
+            },
+        dist: {
+            files: {
+                'skel/src/skel.js': 'skel/src/skel.js',
+                'skel-layers/src/skel-layers.js': 'skel-layers/src/skel-layers.js',
+                'angular-route/angular-route.js': 'angular-route/angular-route.js'
+            }
+        }
     },
 
     // Copies remaining files to places other tasks can use
@@ -375,14 +388,15 @@ module.exports = function (grunt) {
 
     'copy:dist',                  //Copy all files for deployment to the dist folder.
 
-    'copy:fonts',                  //Copy all files for deployment to the dist folder.
+    'bowercopy:dist',
+
+    'copy:fonts',                 //Copy all files for deployment to the dist folder.
 
     'autoprefixer',               //Adds missing browser prefixes to css files.
                                   //leaves newly prefixed files in .tmp/styles
 
     'concat',                     //Concats all javascript and css sourcefiles together.
                                   //scripts.js is made from the app's js files.
-                                  //vendor.js is likewise made from dependencies js files.
                                   //main.css is made from the .tmp/styles/main.css
                                   //vendor.css is likewise made from dependencies css files.
 
