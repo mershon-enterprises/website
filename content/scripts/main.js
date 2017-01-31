6a83197ec40f8d327b74078b8c3bf6c38db182ea
Kevin Mershon <kevin@mershonenterprises.com>
2017-01-30 17:13:03 -0800

    Basic logic to highlight nav menu buttons

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -22,3 +22,19 @@ window.contactUs = function() {
     swal('Uh oh! The mail didn\'t send.', 'We\'ll get it fixed as soon as possible!', 'error');
   });
 };
+
+// highlight the active nav menu button
+$(document).ready(function() {
+  var id = null;
+  switch (window.location.pathname) {
+    case "/about_us.shtml":
+      id = "#menu-about"; break;
+    case "/pricing.shtml":
+      id = "#menu-pricing"; break;
+    case "/solutions.shtml":
+      id = "#menu-solutions"; break;
+    case "/testimonials.shtml":
+      id = "#menu-testimonials"; break;
+  }
+  $(id).addClass("active");
+});
