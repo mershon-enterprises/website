7d168e665f60db59a2e78fa920f6d73af8528817
Kevin Mershon <kevin@mershonenterprises.com>
2017-01-31 22:17:51 -0800

    Implement contact form via Patchwork

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -3,28 +3,40 @@ window.scrollToContact = function() {
   $('html, body').animate({scrollTop: $('#footer').offset().top}, 1000);
 };
 
-window.contactUs = function() {
-  $.ajax({
-    method: 'POST',
-    url: 'scripts/php/contact.php',
-    data: {
-      // FIXME -- pull from contact form
-      // name: $scope.contactInfo.name,
-      // company: $scope.contactInfo.company,
-      // email: $scope.contactInfo.email,
-      // message: $scope.contactInfo.message
-    }
-  })
-  .done(function() {
-    swal('Thank you for contacting us!', 'We\'ll get back to you as soon as possible!', 'success');
-  })
-  .fail(function() {
-    swal('Uh oh! The mail didn\'t send.', 'We\'ll get it fixed as soon as possible!', 'error');
-  });
-};
-
 // highlight the active nav menu button
 $(document).ready(function() {
+  // initialize Patchwork
+  Patchwork.init({
+    accountId: "ACCOUNT_ID",
+    apiKey:    "API_KEY"
+  });
+
+  window.doContact = function() {
+    var data = {
+      name:               $("#contact-form input[name='name']").val(),
+      company:            $("#contact-form input[name='company']").val(),
+      phone:              $("#contact-form input[name='phone']").val(),
+      message:            $("#contact-form textarea[name='message']").val(),
+      to_email_addresses: $("#contact-form input[name='email']").val()
+    };
+    Patchwork.callPlatformMethod({
+      platformId: 1,
+      method: "send",
+      action: "POST",
+      params: data
+    }).then(
+      function() {
+        alert('Thank you for contacting us!', 'We\'ll get back to you as soon as possible!', 'success');
+      },
+      function() {
+        alert('Uh oh! The mail didn\'t send.', 'We\'ll get it fixed as soon as possible!', 'error');
+      }
+    );
+
+    return false;
+  };
+
+  // update navbar
   var id = null;
   switch (window.location.pathname) {
     case "/about_us.shtml":
