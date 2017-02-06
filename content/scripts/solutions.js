77c8d010f5e78ec9973047ca7a0882498707a024
Kevin Mershon <kevin@mershonenterprises.com>
2017-02-06 13:04:58 -0800

    Add 4 price tiers for online bill pay

--- content/scripts/solutions.js
+++ content/scripts/solutions.js
@@ -31,20 +31,41 @@ $(document).ready(function() {
     });
     $("#buy-donation-button-container").append(donationButton);
 
-    var billPayButton = PatchworkStripeButton.createButton({
-      allowBitcoin:          "false",
-      buttonId:              "buy-bill-pay-button",
-      label:                 "Get it now for just $275.00/month",
-      classes:               "button btn btn-block",
-      name:                  "Automatic Bill Pay",
-      description:           "Integrated into your Website",
-      plan:                  "AUTO BILL PAY",
-      amount:                27500,
-      requireBillingAddress: true,
-      panelLabel:            "Pay {{amount}}/month",
-      mode:                  "subscribe",
-    });
-    $("#buy-automatic-bill-pay-button-container").append(billPayButton);
+    var priceTiers = [
+      { amount: 4900,
+        plan: "AUTO BILL PAY 49",
+        div: "#buy-automatic-bill-pay-button-container-1-49"
+      },
+      { amount: 9900,
+        plan: "AUTO BILL PAY 99",
+        div: "#buy-automatic-bill-pay-button-container-50-99"
+      },
+      { amount: 21900,
+        plan: "AUTO BILL PAY 219",
+        div: "#buy-automatic-bill-pay-button-container-100-999"
+      },
+      { amount: 51900,
+        plan: "AUTO BILL PAY 519",
+        div: "#buy-automatic-bill-pay-button-container-1000"
+      }
+    ];
+    for (var i in priceTiers) {
+      var tier = priceTiers[i];
+      var billPayButton = PatchworkStripeButton.createButton({
+        allowBitcoin:          "false",
+        buttonId:              "buy-bill-pay-button-" + tier.amount,
+        label:                 "Start Trial",
+        classes:               "button btn btn-block",
+        name:                  "Automatic Bill Pay",
+        description:           "Integrated into your Website",
+        plan:                  tier.plan,
+        amount:                tier.amount,
+        requireBillingAddress: true,
+        panelLabel:            "Pay {{amount}}/month",
+        mode:                  "subscribe",
+      });
+      $(tier.div).append(billPayButton);
+    }
 
   });
 });
